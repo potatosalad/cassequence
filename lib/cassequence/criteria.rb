4f8c03e0c6c51ce89db816beb647527b0fe5c155
lyon <lyondhill@gmail.com>
2012-07-18 10:00:46 -0600

    more fixes

--- lib/cassequence/criteria.rb
+++ lib/cassequence/criteria.rb
@@ -34,7 +34,7 @@ module Cassequence
       unless self.raw_result      
         validate_hash
         self.raw_result = Cassequence.client.get(self.klass.column_family_name, self.query_hash.delete(:key).to_s, self.query_hash)
-        self.raw_result.values.map! { |json|  json }.to_json
+        self.raw_result = self.raw_result.values.map { |json|  json }.to_json
       end
       self.raw_result
     end
