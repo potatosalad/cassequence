79fd4ccae907617d870ef6a613e6ebb76adb0565
Andrew Bennett <andrew@delorum.com>
2012-09-12 10:50:29 -0600

    Part of the way there on having a schema loaded so specs pass.  Lets see what you do, Travis.

--- Rakefile
+++ Rakefile
@@ -1,7 +1,19 @@
 #!/usr/bin/env rake
 require "bundler/gem_tasks"
 
+namespace :data do
+  desc "Load test data structures."
+  task :load do
+    schema_path = "#{File.expand_path(Dir.pwd)}/spec/support/schema.txt"
+    begin
+      sh("cassandra-cli --host localhost --batch < #{schema_path}")
+    rescue
+      puts "Schema already loaded."
+    end
+  end
+end
+
 require 'rspec/core/rake_task'
-RSpec::Core::RakeTask.new('spec')
+RSpec::Core::RakeTask.new('spec' => 'data:load')
 
 task :default => :spec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,33 @@
-require 'bundler/setup'
+require 'rubygems'
 
-$:.push File.expand_path("../lib", __FILE__)
-require 'pry'
-require 'simplecov'
-SimpleCov.start
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'cassequence'
+require 'rspec'
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(File.dirname(__FILE__), "spec/support/**/*.rb")].each {|f| require f}
 
-ENV['test'] = 'true'
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+end
